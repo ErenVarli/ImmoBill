cd951b75624a7c4e73f35049b3de9e2e74e40c44
Loic <exvsk65@gmail.com>
2024-03-21 15:43:18 +0100

    loic: affichage les biens

--- src/Controller/Admin/AdminBienController.php
+++ src/Controller/Admin/AdminBienController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\BienRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AdminBienController extends AbstractController
 {
-    #[Route('/admin/admin/bien', name: 'app_admin_admin_bien')]
-    public function index(): Response
+    #[Route('/admin/bien', name: 'lesBiensAdmin')]
+    public function index(BienRepository $lesBiens): Response
     {
+        $lesBiens->findAll();
         return $this->render('admin/admin_bien/index.html.twig', [
-            'controller_name' => 'AdminBienController',
+            'lesBiens' => $lesBiens,
         ]);
     }
 }
